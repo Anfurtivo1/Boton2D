fb8d5dba354a06b7d24cb4fb86744c34e013cbe5
Andrés Sánchez Juárez <anfurtivo1@hotmail.com>
2025-10-21 03:00:48 +0200

    Añadidos scripts guardado

--- Assets/Resources/Scripts/Guardado.cs
+++ Assets/Resources/Scripts/Guardado.cs
@@ -2,38 +2,92 @@ using UnityEngine;
 
 public class Guardado : MonoBehaviour
 {
+    public static Guardado instance;
+
     public int userFirstTimeExperience = 0;
-    public float floatStart;
-    public string stringStart;
-    public int retrievedUserFirstTimeExperience;
-    public float retrievedFloat;
-    public string retrievedString;
+    //public float floatStart;
+    //public string stringStart;
+    //public int retrievedUserFirstTimeExperience;
+    //public float retrievedFloat;
+    //public string retrievedString;
 
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        if (PlayerPrefs.HasKey("userFirstTimeExperience"))
+        if (instance == null)
         {
-            retrievedUserFirstTimeExperience = PlayerPrefs.GetInt("retrievedUserFirstTimeExperience");
-            
-
+            instance = this;
+            DontDestroyOnLoad(gameObject);
         }
         else
         {
-            userFirstTimeExperience = 1;
-            PlayerPrefs.SetInt("userFirstTimeExperience", userFirstTimeExperience);
+            Destroy(gameObject);
         }
+    }
 
-        if (PlayerPrefs.HasKey("floatStart") && PlayerPrefs.HasKey("stringStart"))
-        {
-            
-            PlayerPrefs.SetFloat("floatStart", floatStart);
-            PlayerPrefs.SetString("stringStart", stringStart);
 
-            retrievedFloat = PlayerPrefs.GetFloat("floatStart");
-            retrievedString = PlayerPrefs.GetString("retrievedString");
-        }
+    //Start is called once before the first execution of Update after the MonoBehaviour is created
+    //void Start()
+    //{
+    //    if (PlayerPrefs.HasKey("userFirstTimeExperience"))//Ya tenía datos guardados, estos datos se guardan al acabar cada run
+    //    {
+
+    //        CargarDatos();
+    //        retrievedUserFirstTimeExperience = PlayerPrefs.GetInt("retrievedUserFirstTimeExperience");
+
+    //    }
+
+    //    if (PlayerPrefs.HasKey("floatStart") && PlayerPrefs.HasKey("stringStart"))
+    //    {
+
+    //        Guardamos desde PlayerPrefs
+    //        GuardarDatos();
+
+    //        PlayerPrefs.SetFloat("floatStart", floatStart);
+    //        PlayerPrefs.SetString("stringStart", stringStart);
+
+    //        retrievedFloat = PlayerPrefs.GetFloat("floatStart");
+    //        retrievedString = PlayerPrefs.GetString("retrievedString");
+    //    }
+
+
+    //}
+
+    public void CargarDatos()
+    {
+        // Cargamos en PlayerPrefs
+        Player.playerInstance.bullet_Damage = PlayerPrefs.GetInt("bullet_Damage");
+        //PlayerPrefs.GetInt("player_HP");
+        GameManager.Instance.Money_Amount = PlayerPrefs.GetInt("Money_Amount");
+
+        Player.playerInstance.player_AttackRate = PlayerPrefs.GetFloat("player_AttackRate");
+
+        GameManager.Instance.MonsterKills = PrefsDictionary.LoadDictionary("MonsterKills");
+        //Debug.Log("Diccionario cargado desde PlayerPrefs.");
+
+        Debug.Log("Datos cargados");
+    }
+
+
+    public void GuardarDatos()
+    {
+        // Guardamos en PlayerPrefs
+        PlayerPrefs.SetInt("bullet_Damage",Player.playerInstance.bullet_Damage);
+        //PlayerPrefs.SetInt("player_HP", Player.playerInstance.player_HP);
+        PlayerPrefs.SetInt("Money_Amount", GameManager.Instance.Money_Amount);
+
+        PlayerPrefs.SetFloat("player_AttackRate", Player.playerInstance.player_AttackRate);
+
+        PrefsDictionary.SaveDictionary("MonsterKills", GameManager.Instance.MonsterKills);
+        //Debug.Log("Diccionario guardado en PlayerPrefs.");
+
+        userFirstTimeExperience = 1;
+
+        PlayerPrefs.SetInt("userFirstTimeExperience", userFirstTimeExperience);
+
+        Debug.Log("Datos guardados");
+
 
     }
+
+
 }
---
+++ Assets/Resources/Scripts/PrefsDictionary.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class PrefsDictionary
+{
+    public static void SaveDictionary(string key, Dictionary<int, int> dict)
+    {
+        // Lo convertimos a JSON manualmente
+        var data = new DictionaryData(dict);
+        string json = JsonUtility.ToJson(data);
+
+        PlayerPrefs.SetString(key, json);
+        PlayerPrefs.Save();
+
+        Debug.Log($"[PrefsDictionary] Diccionario guardado con clave '{key}'.");
+        Debug.Log($"[PrefsDictionary] Contenido JSON:\n{json}");
+    }
+
+    public static Dictionary<int, int> LoadDictionary(string key)
+    {
+        if (!PlayerPrefs.HasKey(key))
+        {
+            Debug.LogWarning($"[PrefsDictionary] No existe ninguna entrada con la clave '{key}'. Se devuelve un diccionario vacío.");
+            return new Dictionary<int, int>();
+        }
+
+        string json = PlayerPrefs.GetString(key);
+        Debug.Log($"[PrefsDictionary] Cargando diccionario con clave '{key}'.");
+        Debug.Log($"[PrefsDictionary] Contenido JSON cargado:\n{json}");
+
+        var data = JsonUtility.FromJson<DictionaryData>(json);
+        Dictionary<int, int> dict = data?.ToDictionary() ?? new Dictionary<int, int>();
+
+        Debug.Log($"[PrefsDictionary] Diccionario reconstruido con {dict.Count} entradas.");
+        foreach (var kvp in dict)
+        {
+            Debug.Log($"  ID {kvp.Key} → {kvp.Value}");
+        }
+
+        return dict;
+    }
+
+    [System.Serializable]
+    private class DictionaryData
+    {
+        public List<int> keys = new List<int>();
+        public List<int> values = new List<int>();
+
+        public DictionaryData(Dictionary<int, int> dict)
+        {
+            keys = dict.Keys.ToList();
+            values = dict.Values.ToList();
+        }
+
+        public Dictionary<int, int> ToDictionary()
+        {
+            var dict = new Dictionary<int, int>();
+            for (int i = 0; i < keys.Count; i++)
+            {
+                dict[keys[i]] = values[i];
+            }
+            return dict;
+        }
+
+        public DictionaryData() { }
+    }
+}
